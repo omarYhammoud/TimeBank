1fe89b115054f342474bdab6beb59a7cdc01828d
omarYhammoud <omarhammoud.cs@gmail.com>
2026-07-05 23:24:24 +0300

    chore: initial backend setup with MongoDB Atlas, schema, and seed script

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-mongoose.connect("mongodb+srv://TimeBank:YL7YGNGdF3Ww8rRp@timebank.6jlxckl.mongodb.net/?appName=TimeBank");
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+console.log("SERVER FILE LOADED");
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB error:", err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
